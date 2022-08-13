e4c5ad2d0bce8d3658450563a391be09ef3ca7ed
joe <joe.chang1014@gmail.com>
2022-08-13 09:23:28 +0800

    feat gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import poweredBy from "./powered-by-vitawind-dark.png";
-import TodoItem from "./components/TodoItem";
-import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./pages/Todo";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="text-center selection:bg-green-900">
-      <TodoItem />
-      <AiOutlinePlus />
+      <Todo />
     </div>
   );
 }
